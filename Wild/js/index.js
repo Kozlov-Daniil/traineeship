3bc853480c271881f24a1f039d4d807cbee47c21
Kozlov-Daniil <57208983+Kozlov-Daniil@users.noreply.github.com>
2022-12-22 02:01:02 +0300

    Hiding/showing blocks

--- Wild/js/index.js
+++ Wild/js/index.js
@@ -25,9 +25,25 @@ if(counters) {
 }
 
 function isHide() {
-  (document.getElementById('basket').style.display='none')
+    document.getElementById('basket').style.display='none'
+    document.getElementById('closeding1').style.display='flex'
+    document.getElementById('closeding').style.display='none'
+}
+
+function isV() {
+    document.getElementById('basket').style.display='flex'
+    document.getElementById('closeding1').style.display='none'
+    document.getElementById('closeding').style.display='flex'
 }
 
 function isHides() {
-  (document.getElementById('misid').style.display='none')
+    document.getElementById('misid').style.display='none'
+    document.getElementById('closeding2').style.display='none'
+    document.getElementById('closeding3').style.display='flex'
+}
+
+function isVs() {
+    document.getElementById('misid').style.display='block'
+    document.getElementById('closeding2').style.display='flex'
+    document.getElementById('closeding3').style.display='none'
 }
